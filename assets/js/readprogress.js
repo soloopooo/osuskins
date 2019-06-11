344b5d2c961686896d3b07c1b0dd0347857f5a40
soloopooo <soloopooo@foxmail.com>
2019-06-11 19:23:06 +0800

    a

--- assets/js/readprogress.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var dh = $(document).height();
-    var vh = $(window).height();
-    var sHeight = dh - vh;
-    $(window).scroll(function () {  
-      var perc = $(window).scrollTop() / (dh - vh);
-      $('.j-scroll-indicator').css({width: perc * 100 + '%'});
-});  
-}());
