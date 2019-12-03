ca86df81fc76ac99054c2e7633155ae9c737171d
soloopooo <36123081+soloopooo@users.noreply.github.com>
2019-12-03 20:58:31 +0800

    添加滚动条

---
+++ assets/js/progress.js
@@ -0,0 +1,13 @@
+<script>
+    (function(){
+        let pageHeight = document.body.scrollHeight || document.documentElement.scrollHeight; // 页面总高度
+        let windowHeight = document.documentElement.clientHeight || document.body.clientHeight; // 浏览器视口高度
+        let scrollAvail = pageHeight - windowHeight; // 可滚动的高度
+        console.log('可滚动的高度：', scrollAvail);
+        window.onscroll = function () {
+            let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;  // 获取滚动条的高度
+            console.log('滚动条的高度：', scrollTop);
+            document.querySelector('.progress').style.width = (scrollTop/scrollAvail)*100 + '%';    // 计算占比
+        };
+    }());
+</script>
