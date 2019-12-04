425fbf256426488a96aa803e216adddec5e7ef67
soloopooo <36123081+soloopooo@users.noreply.github.com>
2019-12-04 19:09:09 +0800

    Update progress.js

--- assets/js/progress.js
+++ assets/js/progress.js
@@ -1,11 +1,12 @@
-(function(){
-        let pageHeight = document.body.scrollHeight || document.documentElement.scrollHeight; // 页面总高度
-        let windowHeight = document.documentElement.clientHeight || document.body.clientHeight; // 浏览器视口高度
-        let scrollAvail = pageHeight - windowHeight; // 可滚动的高度
-        console.log('可滚动的高度：', scrollAvail);
-        window.onscroll = function () {
-            let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;  // 获取滚动条的高度
-            console.log('滚动条的高度：', scrollTop);
-            document.querySelector('.progressdx').style.width = (scrollTop/scrollAvail)*100 + '%';    // 计算占比
-        };
-    }());
+(function() {
+    var wh = window.innerHeight;//页面高度
+    var h = document.body.getBoundingClientRect().height;//body高度
+
+    var dh = h - wh;
+    window.addEventListener('scroll', function() {
+        window.requestAnimationFrame(function() {
+            var percent = Math.max(0, Math.min(1, window.pageYOffset / dh));
+            document.querySelector(".progressx").style.width = percent * 100 + '%';
+        })
+    }, false);
+})();
