8658282c550fa375252ab7762c728fa9c11456ab
soloopooo <36123081+soloopooo@users.noreply.github.com>
2019-12-04 19:21:11 +0800

    Update progress.js

--- assets/js/progress.js
+++ assets/js/progress.js
@@ -1,7 +1,7 @@
 (function() {
     var wh = window.innerHeight;//页面高度
-    var h = document.getElementsByClass("visible").getBoundingClientRect().height;//body高度
-
+    var hm = document.getElementsByClassName("visible");
+    var h = hm[0].getBoundingClientRect().height;//body高度
     var dh = h - wh;
     window.addEventListener('scroll', function() {
         window.requestAnimationFrame(function() {
