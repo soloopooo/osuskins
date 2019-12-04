9bb347d371b112e3a9bd4cf5ec5a2b78ea95aafa
soloopooo <36123081+soloopooo@users.noreply.github.com>
2019-12-04 20:38:00 +0800

    Update progress.js

--- assets/js/progress.js
+++ assets/js/progress.js
@@ -1,11 +1,12 @@
 (function() {
     var wh = window.innerHeight;//页面高度
-    var h = $('.visible').getBoundingClientRect().height;//body高度
-    var dh = h - wh;
+    var h = document.getElementsByClassName("visible");
+
+    var dh = h[0].getBoundingClientRect.height - wh;
     window.addEventListener('scroll', function() {
         window.requestAnimationFrame(function() {
             var percent = Math.max(0, Math.min(1, window.pageYOffset / dh));
-            document.querySelector(".progressdx").style.width = percent * 100 + '%';
+            document.querySelector("#progressdx").style.width = percent * 100 + '%';
         })
     }, false);
 })();
